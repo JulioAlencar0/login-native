3092b5f853228577ec948382ec49e01d5c6d493e
Júlio <213834242+JulioAlencar0@users.noreply.github.com>
2025-09-17 09:10:47 -0300

    Refactor layout and formatting in Index component for improved readability

--- app/index.tsx
+++ app/index.tsx
@@ -6,27 +6,47 @@ export default function Index() {
   const router = useRouter();
   return (
     <View style={styles.container}>
-    <Image style={styles.image} source={require("../assets/logo-screen.svg")}/>
-    <View style={styles.textBox}>
-      <Text style={styles.title}>Discover Your Dream Job here</Text>
-      <Text style={styles.subtitle}>Explore all the existing job roles based on your interest and study majorWelcome to JobFinder</Text>
+      <Image
+        style={styles.image}
+        source={require("../assets/logo-screen.svg")}
+      />
+      <View style={styles.textBox}>
+        <Text style={styles.title}>Discover Your Dream Job here</Text>
+        <Text style={styles.subtitle}>
+          Explore all the existing job roles based on your interest and study
+          major
+        </Text>
       </View>
       <View style={styles.buttonBox}>
-      <TouchableOpacity style={styles.button} onPress={() => router.push("/login")}><Text style={{color: '#ffffff', fontSize: 17, fontWeight: '600' }}> Login </Text></TouchableOpacity>
-      <TouchableOpacity onPress={() => router.push('/register')} style={styles.button2}><Text style={{color: 'black', fontSize: 17, fontWeight: '600' }}> Register </Text></TouchableOpacity>
-
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => router.push("/login")}
+        >
+          <Text style={{ color: "#ffffff", fontSize: 17, fontWeight: "600" }}>
+            {" "}
+            Login{" "}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => router.push("/register")}
+          style={styles.button2}
+        >
+          <Text style={{ color: "black", fontSize: 17, fontWeight: "600" }}>
+            {" "}
+            Register{" "}
+          </Text>
+        </TouchableOpacity>
       </View>
     </View>
-
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ffffff',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ffffff",
   },
 
   image: {
@@ -35,51 +55,50 @@ const styles = StyleSheet.create({
   },
 
   textBox: {
-    marginLeft:42,
-    marginRight:42,
-    justifyContent: 'center',
-    alignItems: 'center',
+    marginLeft: 42,
+    marginRight: 42,
+    justifyContent: "center",
+    alignItems: "center",
     gap: 10,
   },
 
   title: {
     fontSize: 35,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#1F41BB',
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#1F41BB",
   },
-  
+
   subtitle: {
     fontSize: 14,
-    fontWeight: '400',
-    justifyContent: 'center',
-    textAlign: 'center',
-    color: '#000000',
-    flexDirection: 'row',
+    fontWeight: "400",
+    justifyContent: "center",
+    textAlign: "center",
+    color: "#000000",
+    flexDirection: "row",
   },
-  
+
   buttonBox: {
-    alignItems: 'center',
-    flexDirection: 'row',
+    alignItems: "center",
+    flexDirection: "row",
     gap: 20,
   },
 
   button: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 88,
     width: 160,
     height: 60,
-    backgroundColor: '#1F41BB',
+    backgroundColor: "#1F41BB",
     borderRadius: 10,
-  }, 
+  },
   button2: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 88,
     width: 160,
     height: 60,
-    backgroundColor: '#ffffff',
+    backgroundColor: "#ffffff",
   },
- 
-})
+});
